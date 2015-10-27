aa5f250ef786a4a765f646afbe4caad9201c9626
Jan Mrowiec <jasiek92@gmail.com>
2015-10-27 11:22:15 +0100

    Set root to home action in StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'static_pages/about'
 
   devise_for :users
+
+  root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
