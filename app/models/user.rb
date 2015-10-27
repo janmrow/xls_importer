cbb59556bd88f8939fa8f8935157d5862873fec7
Jan Mrowiec <jasiek92@gmail.com>
2015-10-27 14:10:42 +0100

    Allow users to sign in using username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   before_save :capitalize_names
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, 
+         :validatable, :authentication_keys => [:login]
   
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 8, maximum: 8 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/ }
@@ -13,11 +14,31 @@ class User < ActiveRecord::Base
   validates :expiration_date, presence: true
   validates_date :expiration_date, :after => :today
 
+
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  
   private
 
-	  def capitalize_names
-	    self.first_name = first_name.camelcase
-	    self.last_name = last_name.camelcase
-	  end
+	def capitalize_names
+	  self.first_name = first_name.camelcase
+	  self.last_name = last_name.camelcase
+	end
+  
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions.to_h).first
+    end
+  end
 
 end
